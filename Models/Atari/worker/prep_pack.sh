db124ff8fd8e0ec6d112537f5ded5655f9af8bc9
Ahsan Ali <aali@nevada.unr.edu>
2021-06-04 17:55:03 -0700

    update code

--- Models/Atari/worker/prep_pack.sh
+++ Models/Atari/worker/prep_pack.sh
@@ -7,4 +7,10 @@ sudo docker tag lambda-tensorflow-example:latest 100676110906.dkr.ecr.us-east-1.
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 100676110906.dkr.ecr.us-east-1.amazonaws.com
 sudo docker push 100676110906.dkr.ecr.us-east-1.amazonaws.com/lambda-tensorflow-example:latest
 aws lambda update-function-code --region us-east-1 --function-name tensorflow-endpoint --image-uri 100676110906.dkr.ecr.us-east-1.amazonaws.com/lambda-tensorflow-example:latest
-												  
+			
+
+aws ecr get-login-password --region ${aws_region} \
+        | docker login \
+        --password-stdin \
+        --username AWS \
+        "${aws_account}.dkr.ecr.${aws_region}.amazonaws.com/${repository}									  
